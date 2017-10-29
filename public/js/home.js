c6fcac406918f07faa33b390baaf3b7eae9e9624
Jon Ramer <jramer87@gmail.com>
2017-10-29 10:49:14 -0500

    Swtiches scroll animations on home page from pixel values to percentage values for better mobile experience

--- public/js/home.js
+++ public/js/home.js
@@ -1,47 +1,48 @@
 'use strict';
 $(document).ready(() => {
   // adds 'opaque' class to the navbar.
-  $(window).scroll(() => {
-    const scroll = $(window).scrollTop();
+  //
 
-    if (scroll > 150) {
-      $('nav').addClass('opaque')
-    } else {
-      $('nav').removeClass('opaque')
-    }
+  $(window).on('scroll', function(){
+    let s = $(window).scrollTop(),
+        d = $(document).height(),
+        c = $(window).height();
+
+    let scrollPercent = (s / (d - c)) * 100;
+
+
+      if (scrollPercent > 3) {
+        $('nav').addClass('opaque')
+      } else {
+        $('nav').removeClass('opaque')
+      }
+
+      if (scrollPercent > 4) {
+        $('h2').addClass('fadeInDown')
+        $('h2').addClass('showme')
+      }
+
+      // const scroll = $(window).scrollTop();
+
+      if (scrollPercent > 7) {
+        $('#step-one').addClass('fadeInLeft')
+        $('#step-one').addClass('showme')
+      }
+      // const scroll = $(window).scrollTop();
+
+      if (scrollPercent > 37) {
+        $('#step-two').addClass('fadeInRight')
+        $('#step-two').addClass('showme')
+      }
+      // const scroll = $(window).scrollTop();
+
+      if (scrollPercent > 66) {
+        $('#step-three').addClass('fadeInLeft')
+        $('#step-three').addClass('showme')
+      }
+
+    console.clear();
+    console.log("Scroll Percentage: ", scrollPercent);
   })
 
-  $(window).scroll(() => {
-    const scroll = $(window).scrollTop();
-
-    if (scroll > 5) {
-      $('h2').addClass('fadeInDown')
-      $('h2').addClass('showme')
-    }
-  })
-
-  $(window).scroll(() => {
-    const scroll = $(window).scrollTop();
-
-    if (scroll > 100) {
-      $('#step-one').addClass('fadeInLeft')
-      $('#step-one').addClass('showme')
-    }
-  })
-  $(window).scroll(() => {
-    const scroll = $(window).scrollTop();
-
-    if (scroll > 650) {
-      $('#step-two').addClass('fadeInRight')
-      $('#step-two').addClass('showme')
-    }
-  })
-  $(window).scroll(() => {
-    const scroll = $(window).scrollTop();
-
-    if (scroll > 1100) {
-      $('#step-three').addClass('fadeInLeft')
-      $('#step-three').addClass('showme')
-    }
-  })
 });
